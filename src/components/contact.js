adba9a3963695f952a63acd16a63f7b1aa03d35f
Tenyson Partridge <tenysonpartridge@gmail.com>
2018-10-10 17:50:00 -0600

    added new styles and changed background color to soft silver

--- src/components/contact.js
+++ src/components/contact.js
@@ -25,16 +25,12 @@ export default class Contact extends Component {
         <div className="contact-form" id="contact">
             <form onSubmit={this.onFormSubmit} action="form">
 
-                <label >First name</label>
                 <input type="text" id="fname" name="firstname" placeholder="First Name" value={this.state.fname} onChange={event => this.setState({fname: event.target.value})} />
 
-                <label >Last name</label>
                 <input type="text" id="lname" name="lastname" placeholder="Last Name" value={this.state.lname} onChange={event => this.setState({lname: event.target.value})}/>
 
-                <label >Email</label>
                 <input type="text" id="email" name="lastname" placeholder="Email" value={this.state.email} onChange={event => this.setState({email: event.target.value})}/>
 
-                <label >Subject</label>
                 <textarea name="subject" id="subject" placeholder="Message" value={this.state.subject} onChange={event => this.setState({subject: event.target.value})}/>
 
                 <input type="submit" value="Submit" />
